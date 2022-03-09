1db64ceca12ce77f62edd38f64b125bb5a4c618f
Christopher H. Jordan <christopherjordan87@gmail.com>
2022-03-09 11:05:13 +0800

    Remove unnecessary depedencies from CUDA crate.
    Seems to speed up compilation!

--- cuda/src/lib.rs
+++ cuda/src/lib.rs
@@ -9,19 +9,15 @@
 // This is needed only because tests inside bindgen-produced files (model_*.rs
 // and memory_*.rs) trigger the warning.
 #![allow(deref_nullptr)]
-// Link hyperdrive_cu produced from build.rs
-#![link(name = "hyperdrive_cu", kind = "static")]
-
-pub use mwa_hyperdrive_beam::{CudaError, DevicePointer};
 
 // Import Rust bindings to the CUDA code specific to the precision we're using,
 // and set corresponding compile-time types.
-mwa_hyperdrive_common::cfg_if::cfg_if! {
-    if #[cfg(feature = "cuda-single")] {
-        include!("model_single.rs");
-        include!("memory_single.rs");
-    } else {
-        include!("model_double.rs");
-        include!("memory_double.rs");
-    }
-}
+#[cfg(feature = "cuda-single")]
+include!("model_single.rs");
+#[cfg(feature = "cuda-single")]
+include!("memory_single.rs");
+
+#[cfg(not(feature = "cuda-single"))]
+include!("model_double.rs");
+#[cfg(not(feature = "cuda-single"))]
+include!("memory_double.rs");
